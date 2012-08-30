568d23509cfd7e9a2cae184b2cbe6a0df80c59f3
Mike AbiEzzi <mjezzi@gmail.com>
2012-08-29 20:09:22 -0600

    send email with attachment

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,6 +2,7 @@ class UserMailer < ActionMailer::Base
   default from: "d6cdad1d4cdcbd9f7358@cloudmailin.net"
   
   def summary
-    mail(:to => "david@quickleft.com", :subject => "Test email")
+    attachments['page.pdf'] = PdfGenerator.generate_pdf("http://www.amazon.com")
+    mail(:to => "david@quickleft.com", :subject => "Test email" )
   end
 end
