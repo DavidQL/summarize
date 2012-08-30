85831878884f4a70393ba8ea15e06502dc0b5d4c
Mike AbiEzzi <mjezzi@gmail.com>
2012-08-29 20:45:23 -0600

    whatever

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,7 +2,11 @@ class UserMailer < ActionMailer::Base
   default from: "d6cdad1d4cdcbd9f7358@cloudmailin.net"
   
   def summary
-    attachments['page.pdf'] = PdfGenerator.generate_pdf("http://www.amazon.com")
+    client = Pdfcrowd::Client.new("mjezzi", "538096745e9ed20ba1e046438c55a11b")
+    pdf = client.convertURI(url)
+    File.delete "page.pdf"
+    File.open("page.pdf",'wb') { |f| f.write pdf }
+    attachments["page.pdf"] = File.read("page.pdf")
     mail(:to => "david@quickleft.com", :subject => "Test email" )
   end
 end
